1ca39f01bec41f25dbc3cb595aab7a4b36d94d3d
Stefan Sinca <stefisinca@gmail.com>
2021-12-15 20:41:21 +0200

    second commit ThinQ 3.0

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 // Typewriting module for MAIN
 const mainMessage = document.querySelector('.main-message');
+const getMain = document.querySelector('.main');
 
 let message = ['Take a look at my projects'];
 let textPosition = 0;
@@ -27,13 +28,14 @@ const messageMain = document.querySelector('.main-img');
 
 // Mouse Over & Mouse Out functions
 
-const mouseOver = (imgSrc) => {
+const mouseOver = (imgSrc, text) => {
     mainMessage.style.display = 'none';
     mainImg.src = imgSrc;
     messageMain.style.display = 'flex';
     mainImg.style.display = 'inline-flex';
     htmlDescription.style = 'width: 35%; margin: 3em 2.5em;font-size: 2.5em;line-height: 1.5;text-align: left';
     htmlDescription.style.display = 'block'
+    htmlDescription.innerHTML = text;
 
 }
 
@@ -46,20 +48,22 @@ const mouseOut = () => {
     htmlDescription.style.display = 'none';
 }
 
+
+
 // HTML Project Button
-const htmlButtonIn = () => mouseOver('img/main-img.jpg');
+let htmlButtonMessage = 'Slow and well or fast and decent? NOW can be fast and excellence.'
+const htmlButtonIn = () => mouseOver('img/automation-img.png', htmlButtonMessage);
 const htmlButtonOut = () => mouseOut();
 
 // CSS Project Button
-const cssButtonIn = () => mouseOver('img/css-img.jpeg');
+let cssButtonMessage = 'Leave all to algorithms. Take your work to perfection.'
+const cssButtonIn = () => mouseOver('img/algorithms-img.png', cssButtonMessage);
 const cssButtonOut = () => mouseOut();
 
 // JavaScript Button 
-const jsButtonIn = () => mouseOver('img/js-img.jpg');
+let javaScriptMessage = 'The power of data means authencity. We guarantee authenticity.'
+const jsButtonIn = () => mouseOver('img/authenticity.png', javaScriptMessage);
 const jsButtonOut = () => mouseOut();
-
-
-
 
 
 
